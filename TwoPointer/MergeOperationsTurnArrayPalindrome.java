6885b91a842520f2b41036d8376d41b4bfd38c96
Cathy Qin <qin.xuj@northeastern.edu>
2024-12-28 23:20:48 -0800

    two pointers practice

---
+++ TwoPointer/MergeOperationsTurnArrayPalindrome.java
@@ -0,0 +1,31 @@
+public class MergeOperationsTurnArrayPalindrome {
+  public int minimumOperations(int[] nums) {
+    int left = 0, right = nums.length - 1;
+    int leftSum = nums[left], rightSum = nums[right];
+    int ret = 0;
+
+    while (left < right) {
+      if(leftSum < rightSum) {
+        left++;
+        leftSum += nums[left];
+        ret++;
+      } else if(leftSum > rightSum) {
+        right--;
+        rightSum += nums[right];
+        ret++;
+      } else {
+        left++;
+        right--;
+        leftSum += nums[left];
+        leftSum -= nums[right];
+      }
+    }
+    return ret;
+  }
+
+  public static void main(String[] args) {
+    MergeOperationsTurnArrayPalindrome m =  new MergeOperationsTurnArrayPalindrome();
+    System.out.println(m.minimumOperations(new int[]{4,3,2,1,2,3,1}));
+    System.out.println(m.minimumOperations(new int[]{1,2,3,4}));
+  }
+}
