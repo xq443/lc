803da2effbbcb3b4f4da29afa4f3976c2dc832a2
Cathy Qin <qin.xuj@northeastern.edu>
2025-01-21 14:18:59 -0800

    dp bst

--- DP/Basic/UniqueBST.java
+++ DP/Basic/UniqueBST.java
@@ -19,3 +19,10 @@ public class UniqueBST {
     System.out.println(u.numTrees(n));
   }
 }
+
+// follow up: catalan
+// 1. n nodes, how many structures of binary trees -> h(n)
+// 2. n pairs of parenthesis, how many valid sequences -> h(n) (......)......
+// 3. full binary tree: for all nodes, they either have no children or two children, 2n + 1 nodes,
+// how many structures of full bs can it construct?  -> h(n)
+
