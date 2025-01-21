19f405f23bba408fa5ae54b7fc36237d68ad900b
Cathy Qin <qin.xuj@northeastern.edu>
2025-01-21 14:12:38 -0800

    dp bst

---
+++ DP/Basic/UniqueBST.java
@@ -0,0 +1,21 @@
+package Basic;
+
+public class UniqueBST {
+  public int numTrees(int n) {
+    int[] dp = new int[n + 1];
+    dp[0] = 1;
+
+    for (int i = 1; i <= n; i++) {
+      for (int j = 0; j < i; j++) {
+        dp[i] += dp[j] * dp[i - j - 1];
+      }
+    }
+    return dp[n];
+  }
+
+  public static void main(String[] args) {
+    UniqueBST u = new UniqueBST();
+    int n = 3;
+    System.out.println(u.numTrees(n));
+  }
+}
