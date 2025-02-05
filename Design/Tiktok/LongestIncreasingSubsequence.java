4f6f371a77d5cf73aa8425ff7c6e64064e21af32
Cathy Qin <qin.xuj@northeastern.edu>
2025-02-04 16:16:25 -0800

    review

---
+++ Design/Tiktok/LongestIncreasingSubsequence.java
@@ -0,0 +1,50 @@
+package Tiktok;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LongestIncreasingSubsequence {
+  public int longestIncreasingSubsequence(int[] nums) {
+    int n = nums.length;
+    int[] dp = new int[n];
+    for (int i = 0; i < n; i++) {
+      dp[i] = 1;
+      for(int j = 0; j < i; j++) {
+        if(nums[i] > nums[j]) {
+          dp[i] = Math.max(dp[i], dp[j] + 1);
+        }
+      }
+    }
+    int ret = 0;
+    for(int i = 0; i < n; i++) {
+      ret = Math.max(ret, dp[i]);
+    }
+    return ret;
+  }
+
+  public int longestIncreasingSubsequence_binarySearch(int[] nums) {
+    List<Integer> ret = new ArrayList<>();
+    for(int num : nums) {
+      int pos = binarySearch(ret, num); // the leftmost/ first index that the num fits
+      if(pos == ret.size()) ret.add(num);
+      else ret.set (pos, num);
+    }
+    return ret.size();
+  }
+
+  private int binarySearch(List<Integer> ret, int num) {
+    int left = 0, right = ret.size() - 1;
+    while(left <= right) {
+      int mid = left + (right - left) / 2;
+      if(num < ret.get(mid)) left = mid + 1;
+      else right = mid - 1;
+    }
+    return left;
+  }
+
+  public static void main(String[] args) {
+    LongestIncreasingSubsequence l  = new LongestIncreasingSubsequence();
+    System.out.println(l.longestIncreasingSubsequence(new int[]{10,9,2,5,3,7,101,18}));
+    System.out.println(l.longestIncreasingSubsequence_binarySearch(new int[]{10,9,2,5,3,7,101,18}));
+  }
+}
---
+++ Design/Tiktok/MinWindowSubstring.java
@@ -0,0 +1,43 @@
+package Tiktok;
+
+public class MinWindowSubstring {
+  public String minWindow(String s, String t) {
+    if(t.length() > s.length()) return "";
+
+    int[] map = new int[256];
+    int left = 0, right = 0;
+    int ret = Integer.MAX_VALUE;
+    for(char ch : t.toCharArray()) {
+      map[ch]++;
+    }
+    int start = 0;
+    int cnt = t.length();
+    char[] charS = s.toCharArray();
+
+    while(right < charS.length) {
+      // expand the window
+      if(map[charS[right]] > 0)  cnt--;
+      map[charS[right]]--;
+      right++;
+
+      // contract the window
+      while(cnt == 0) {
+        if(right - left < ret) {
+          ret = right - left;
+          start = left;
+        }
+
+        if(map[charS[left]] == 0) cnt++;
+        map[charS[left]]++;
+        left++;
+      }
+    }
+    return ret == Integer.MAX_VALUE ? "" : s.substring(start, start + ret);
+  }
+
+  public static void main(String[] args) {
+    MinWindowSubstring m = new MinWindowSubstring();
+    String s = "ADOBECODEBANC", t = "ABC";
+    System.out.println(m.minWindow(s, t));
+  }
+}
