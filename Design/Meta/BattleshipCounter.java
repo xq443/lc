543f11770ca459fd4a6c366814838b5df8e158cd
Cathy Qin <qin.xuj@northeastern.edu>
2025-05-09 21:00:59 -0700

    meta review

--- Design/Meta/BattleshipCounter.java
+++ Design/Meta/BattleshipCounter.java
@@ -1,7 +1,7 @@
 package Meta;
 
 public class BattleshipCounter {
-  public static int countBattleships(char[][] board) {
+  public int countBattleships(char[][] board) {
     int m = board.length;
     int n = board[0].length;
     boolean[][] visited = new boolean[m][n];
@@ -9,18 +9,17 @@ public class BattleshipCounter {
 
     for (int i = 0; i < m; i++) {
       for (int j = 0; j < n; j++) {
-        if (board[i][j] == 'X' && !visited[i][j]) {
+        if (board[i][j] == 'X') {
           if (isValidShip(board, visited, i, j)) {
             count++;
           }
         }
       }
     }
-
     return count;
   }
 
-  private static boolean isValidShip(char[][] board, boolean[][] visited, int i, int j) {
+  private boolean isValidShip(char[][] board, boolean[][] visited, int i, int j) {
     int m = board.length, n = board[0].length;
 
     // Check horizontal ship of length 3
@@ -42,13 +41,61 @@ public class BattleshipCounter {
     return false;
   }
 
+
+  private static final int[][] DIRECTIONS = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+
+  public int countBattleships_dfs(char[][] grid) {
+    if (grid == null || grid.length == 0 || grid[0].length == 0) {
+      return 0;
+    }
+
+    int count = 0;
+    int rows = grid.length;
+    int cols = grid[0].length;
+    boolean[][] visited = new boolean[rows][cols];
+
+    for (int i = 0; i < rows; i++) {
+      for (int j = 0; j < cols; j++) {
+        if (grid[i][j] == 'X' && !visited[i][j]) {
+          // Explore in all four directions
+          for (int[] dir : DIRECTIONS) {
+            int length = dfs(grid, visited, i, j, dir[0], dir[1]);
+            if (length == 3) {
+              count++;
+              break; // Only count once per ship
+            }
+          }
+        }
+      }
+    }
+
+    return count;
+  }
+
+  private int dfs(char[][] grid, boolean[][] visited, int i, int j, int di, int dj) {
+    if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length ||
+        grid[i][j] != 'X' || visited[i][j]) {
+      return 0;
+    }
+
+    visited[i][j] = true;
+    int length = 1;
+
+    // Continue in the given direction
+    length += dfs(grid, visited, i + di, j + dj, di, dj);
+
+    return length;
+  }
+
   public static void main(String[] args) {
+    BattleshipCounter b = new BattleshipCounter();
     char[][] board = {
-        {'X', '.', '.', 'X'},
-        {'X', 'X', 'X', 'X'},
-        {'X', '.', '.', 'X'}
+        {'X', '.', 'X', '.'},
+        {'X', 'X', 'X', '.'},
+        {'X', '.', 'X', '.'}
     };
-    int count = countBattleships(board);
+    int count = b.countBattleships(board);
+    //int count2 = b.countBattleships_dfs(board);
     System.out.println("Number of ships of length 3: " + count);
   }
 }
