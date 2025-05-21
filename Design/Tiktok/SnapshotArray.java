52babdf0e2b7bc0bbfb060ea1e205092586a981a
Cathy Qin <qin.xuj@northeastern.edu>
2025-05-20 21:48:23 -0700

    snapshot review

--- Design/Tiktok/SnapshotArray.java
+++ Design/Tiktok/SnapshotArray.java
@@ -48,5 +48,7 @@ public class SnapshotArray {
     System.out.println(snapshotArr.snap()); // Take a snapshot, return snap_id = 0
     snapshotArr.set(0,6);
     System.out.println(snapshotArr.get(0,0)); // Get the value of array[0] with snap_id = 0, return 5
+    System.out.println(snapshotArr.get(0,1)); // 6
+    System.out.println(snapshotArr.get(0,2)); // 6
   }
 }
